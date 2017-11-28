900c314fe8119cde557d46020f04ba52af70a800
Daniel Roach <dannycr09@hotmail.com>
2017-11-27 19:51:28 -0500

    Made initial home page prototype Incorporating Blueprint React UI framework

--- grr-web-app/src/index.js
+++ grr-web-app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Navbar from './'
 import registerServiceWorker from './registerServiceWorker';
+import Homepage from "./Homepage/Homepage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Homepage />, document.getElementById('root'));
 registerServiceWorker();
