2e25551cce53b2e0f4edb95af24ac9acdfa85d90
Daniel Roach <dannycr09@hotmail.com>
2017-11-27 22:40:06 -0500

    Removing unnecessary files from create-react-app call

--- grr-web-app/src/index.js
+++ grr-web-app/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import Navbar from './'
 import registerServiceWorker from './registerServiceWorker';
-// import Homepage from "./Homepage/Homepage";
 import AssetMgmt from "./AssetMgmt/AssetMgmt";
 
 ReactDOM.render(<AssetMgmt />, document.getElementById('root'));
