dcc6d4dacb4bbedd4da8c9ab26383fa56003dfe3
lance13c <pianoman13c@gmail.com>
2018-01-25 14:08:00 -0500

    testing loaders for scss

--- grr-ui/src/index.js
+++ grr-ui/src/index.js
@@ -1,6 +1,8 @@
-import AssetMgmt from "./AssetMgmt/AssetMgmt";
+import AssetMgmt from "./templates/AssetMgmt/AssetMgmt";
+import style from 'style-loader!css-loader!scss-loader!./scss/main.scss';
 var ReactDOM = require('react-dom');
 var React = require('react');
 
+let root = document.getElementById('root');
 
-ReactDOM.render(<AssetMgmt />, document.getElementById('root'));
+ReactDOM.render(<AssetMgmt />, root);
