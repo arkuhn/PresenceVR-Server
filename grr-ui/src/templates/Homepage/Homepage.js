1e725f152118d20d76e103126096c1c4ae4e2814
Arsh <arsh2223@gmail.com>
2018-03-29 19:17:13 -0400

    Fixing buttons

--- grr-ui/src/templates/Homepage/Homepage.js
+++ grr-ui/src/templates/Homepage/Homepage.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import {Button} from '@blueprintjs/core'
 import './Homepage.css';
 import { Link } from 'react-router-dom';
+import GRRNavBar from "../GRRNavBar/GRRNavBar";
 
 class Homepage extends Component {
     render() {
         return (
             <div className="Homepage">
+                <GRRNavBar/>
                 <div className="center-content">
                     <h1 id="logo">Game Room Recruiting</h1>
                     <div className="pt-button-group pt-vertical">
-                        <Link to="/"><button id="vert-group-button fullwidth">Dashboard</button></Link>
-                        <Link to="/rooms"><button id="vert-group-button fullwidth">Rooms</button></Link>
-                        <Link to="/vrcall"><button id="vert-group-button fullwidth">Video</button></Link>
+                        <Link to="/"><Button className="pt-intent-primary vert-group-button">Dashboard</Button></Link>
+                        <Link to="/rooms"><Button className="pt-intent-primary vert-group-button">Rooms</Button></Link>
+                        <Link to="/vrcall"><Button className="pt-intent-primary vert-group-button">Video</Button></Link>
                         
                     </div>
                 </div>
