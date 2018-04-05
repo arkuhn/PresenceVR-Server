ee7a91c46fe09129d6b6b8f6f2a1b7a8362bc440
Allen Liu <akl2390@rit.edu>
2018-04-05 13:44:58 -0400

    Updated homepage

--- grr-ui/src/templates/Homepage/Homepage.js
+++ grr-ui/src/templates/Homepage/Homepage.js
@@ -10,42 +10,40 @@ class Homepage extends Component {
             <div className="Homepage">
                 <GRRNavBar/>
                 <h1 className="centered" id="header-text">Game Room Recruiting</h1>
-                    <div className="flex-container">
+                <h3 className="centered" id="header-text">The future of remote interviewing</h3>
+                    <div className="homepage-container">
 
                         <div className="options-container">
-                            <div className="pt-vertical full-width">
-                                <h2 className="centered full-width">Feature Guide</h2>
-                                <ul class="font-size-adjust">
-                                    <li>You can create or join a room from the Rooms List page.</li>
-                                    <li>Click the example video call to view a default room.</li>
-                                    <li>From any other page, click the "Game Room Recruiting" button in the top left to return here.</li>
-                                </ul>
+                            <div className="pt-vertical full-width options-container fixed-size vert-align">
+                                <p className="text-settings">
+                                    We've created a system that aims to provide an immersive experience that
+                                    emulates physical interviews. It will allow interviewers and interviewees to
+                                    stream themselves to each other in a customizeable virtual reality space, and 
+                                    display uploadable assets such as past work or accomplishments. 
+                                </p>
                             </div>
                         </div>
 
-                        <div className="options-container">
-                            <div className="rooms-container" data-reference="pt-card">
-                            <div className="rooms-container-wrapper">
-                                <div className="card-wrapper" >
-                                    <Card interactive={true} elevation={Card.ELEVATION_TWO}>
-                                        <h4 className="centered"><a href="#">Rooms</a></h4>
-                                            <Link to="/rooms"><Button className="pt-intent-primary vert-group-button full-width">View Room List</Button></Link>
-                                    </Card>
-                                </div>
+                        <div className="options-container grey-bg">
+                            <div className="fixed-size block-center vert-align">
+                                <h2 className="centered">Rooms</h2>
+                                <p className="text-settings">
+                                    From here, you can create a new room starting with our provided template, <br />
+                                    or join an existing room that you've been invited to.
+                                </p>
+                                <Link to="/rooms"><Button className="pt-intent-primary vert-group-button full-width">View Room List</Button></Link>
                             </div>
-                            </div>
+                            
                         </div>
 
                         <div className="options-container">
-                            <div className="rooms-container" data-reference="pt-card">
-                            <div className="rooms-container-wrapper">
-                                <div className="card-wrapper" >
-                                    <Card interactive={true} elevation={Card.ELEVATION_TWO}>
-                                        <h4 className="centered"><a href="#">Example Video Call</a></h4>
-                                            <Link to="/rooms/default"><Button className="pt-intent-primary vert-group-button full-width">View</Button></Link>
-                                    </Card>
-                                </div>
-                            </div>
+                            <div className="fixed-size block-center vert-align">
+                                <h2 className="centered">The Team</h2>
+                                <p className="text-settings">
+                                    Team DAAD: Dominic Cicilio, Arshdeep Khalsa, Allen Liu, Daniel Roach <br />
+                                    Sponsor: Jim Bondi <br />
+                                    Faculty Coach: Yasmine El-glaly
+                                </p>     
                             </div>
                         </div>
                         
