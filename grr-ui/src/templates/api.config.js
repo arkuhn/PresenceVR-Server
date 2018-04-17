5b0aeb67b26d4e7500137cecee47ff191cf294fa
lance13c <pianoman13c@gmail.com>
2018-04-17 17:43:34 -0400

    quick merge config fix

--- grr-ui/src/templates/api.config.js
+++ grr-ui/src/templates/api.config.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export const API_URL = 'http://localhost:8080';
-
-=======
-export const API_URL = 'localhost:8080'; //'https://gameroomrecruiting.herokuapp.com';
->>>>>>> f7e1809acbe23ea638b341259c7bc8eb20eb504d
