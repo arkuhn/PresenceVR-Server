ecab22039a403f18c44e08136d231cb0b6ba5f4a
Austin Wolf <austinwolf@Austins-MacBook-Pro.local>
2018-10-04 10:14:47 -0400

    Added Calendar

--- grr-ui/src/templates/Homepage/Homepage.js
+++ grr-ui/src/templates/Homepage/Homepage.js
@@ -4,23 +4,24 @@ import './Homepage.css';
 import { Link } from 'react-router-dom';
 import GRRNavBar from "../GRRNavBar/GRRNavBar";
 import vrimage from "../../assets/images/vrimage.png";
+import Calendar from 'react-calendar';
 import PresenceVRNavBar from "../PresenceVRNavBar/PresenceVRNavBar"
-import { Button, Header, Icon, Grid, Modal, Menu, Label } from 'semantic-ui-react';
+import { Button, Header, Icon, Grid, Modal, Menu, List} from 'semantic-ui-react';
 
 
 class Homepage extends Component {
     
     appointmentButton () {
         return (
-            <Modal color='grey' trigger={ <Menu.Item>Select Appointment</Menu.Item> } closeIcon>
+            <Modal color='grey' trigger={ <Menus.Item>Select Appointment</Menu.Item> } closeIcon>
                 <Header icon='user outline' content='Appointment information' />
                 <Modal.Content>
-                    <Label>
+                    <List>
                             Date
-                            <Label.Detail>Today</Label.Detail><br/>
+                            <List.Detail>Today</List.Detail><br/>
                             Participant
-                            <Label.Detail>Tony</Label.Detail><br/>
-                        </Label>
+                            <List.Detail>Tony</List.Detail><br/>
+                        </List>
                     <Button as={Link} to="/interview" fluid width={4}>Join</Button>
                     <Button fluid width={4}>Edit</Button>
                     <Button fluid width={4}>Cancel</Button>
@@ -48,64 +49,18 @@ class Homepage extends Component {
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{height:"100vh"}}>
-                        <Grid.Column width={4} >
-                            <div border-style='solid' border-width='medium' border-color='black'>
-                                <Header as='h3' textAlign='center'>Upcoming Interviews</Header>
-                                {this.appointmentButton()}
-                            </div>
-                            
+                        <Grid.Column width={4} divided={true}>
+                            <Header as='h3' textAlign='center'>Upcoming Interviews</Header>
+                            {this.appointmentButton()}     
                         </Grid.Column>
-                        <Grid.Column width={12} color='green'>
+                        <Grid.Column width={12} divided={true}>
                             <Header as='h3' textAlign='center'>calendar</Header>
-                            {this.appointmentButton()}
+                            <Calendar />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
         );
-        /*
-        return (
-            <div className="Homepage">
-                <GRRNavBar/>
->>>>>>> Updated Homepage
-                <h1 className="centered" id="header-text">Game Room Recruiting</h1>
-                <h3 className="centered" id="header-text">The future of remote interviewing</h3>
-                    <div className="homepage-container">
-
-                        <div>
-                            <div className="block-center">
-                                <img src={"/assets/images/vrimage.png"} alt="vr image" />
-                            </div>
-                        </div>
-
-                        <div className="grey-bg">
-                            <div className="block-center fixed-size vert-align">
-                                <h2 className="centered">Get Started</h2>
-                                <p className="text-settings">
-                                    To create or join a call, start by navigating to the room section below. Once 
-                                    you've entered a room, you can edit the settings and upload assets as you see fit. 
-                                    Make sure VR mode is toggled on to get the full experience. If you're 
-                                    ever lost, remember you can click the button in the top left to return here!
-                                </p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="fixed-size block-center vert-align">
-                                <h2 className="centered">Rooms</h2>
-                                <p className="text-settings">
-                                    From here, you can create a new room starting with our provided template, 
-                                    or join an existing room that you've been invited to. 
-                                </p>
-                                <Link to="/rooms"><Button className="pt-intent-primary vert-group-button full-width">View Room List</Button></Link>
-                            </div>                       
-                        </div>
-                        
-                    </div>
-                
-            </div>
-        );
-        */
     }
 }
 
