94c153c81e1f76e0d07960fe959623b89376d9be
Jonathan Ding <jxd3700@g.rit.edu>
2018-10-16 15:30:02 -0400

    edit/delete/create hitting apis

--- grr-ui/src/templates/Homepage/createInterview.js
+++ grr-ui/src/templates/Homepage/createInterview.js
@@ -2,50 +2,74 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PresenceVRNavBar from "../PresenceVRNavBar/PresenceVRNavBar"
 import { Button, Header, Input, Grid, Modal, List, Image} from 'semantic-ui-react';
-import axios from 'axios';
-import {API_URL} from "../api.config";
+import ConsumeInterview from './consumeInterview'
 
-const createInterviewRequest = () => {
-    //TODO take values from forum inputs
-    axios.post(API_URL + '/api/interviews/', {
-        host: 'jaaw1001@rit.edu',
-        subject: 'tests',
-        occursOnDate: '12/12/1221',
-        occursAtTime: '8:31:22',
-        participants: 'noone@email.com'
-    }).then((result) => {
-        console.log('got a result')
-        console.log(result)
-    });
-}
+class CreateInterview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.consumeInterview = new ConsumeInterview()
+        this.state = {dateValue: props.date,
+                      timeValue: '',
+                      participantsValue: '',
+                      subjectValue: ''};
+    
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.handleParticipantsChange = this.handleParticipantsChange.bind(this);
+        this.handleSubjectChange = this.handleSubjectChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+    handleDateChange(event) {
+        this.setState({dateValue: event.target.value})
+    }
 
-const CreateInterview = () => {
-    return (
-        <Modal size='mini' trigger={ 
-            <Button fluid attached='bottom'>Create an interview</Button>
-         }>
-        <Header icon='alternate calendar outline' content='Interview Details' />
-        <Modal.Content>
-                    <List>
-                        <List.Item>
-                            <Input fluid label='Date' placeholder='MM/DD/YYYY' />
-                        </List.Item>
-                        <List.Item>
-                            <Input fluid label='Time' placeholder='HH:MM:SS'/>
-                        </List.Item>
-                        <List.Item>
-                            <Input fluid label='Participants' placeholder='participant1@email.co' />
-                        </List.Item>
-                        <List.Item>
-                            <Input fluid label='Subject' placeholder='Art interview' />
-                        </List.Item>
-                    </List>
-                    <Button primary onClick={createInterviewRequest}>Create</Button>
-                    <Button secondary>Cancel</Button>
-                </Modal.Content>
-    </Modal>
-    )
+    handleTimeChange(event) {
+        this.setState({timeValue: event.target.value})
+    }
+
+    handleParticipantsChange(event) {
+        this.setState({participantsValue: event.target.value})
+    }
+
+    handleSubjectChange(event) {
+        this.setState({subjectValue: event.target.value})
+    }
+  
+    handleSubmit(event) {
+      this.consumeInterview.updateInterview(0)
+      event.preventDefault();
+    }
+
+    render() {
+        return (
+            <Modal size='mini' trigger={ 
+                <button class='ui circular icon button' role='button'>
+                    <i aria-hidden='true' class='add icon' />
+                </button>
+            }>
+            <Header icon='alternate calendar outline' content='Interview Details' />
+            <Modal.Content>
+                        <List>
+                            <List.Item>
+                                <Input fluid label='Date' placeholder='MM/DD/YYYY' />
+                            </List.Item>
+                            <List.Item>
+                                <Input fluid label='Time' placeholder='HH:MM:SS'/>
+                            </List.Item>
+                            <List.Item>
+                                <Input fluid label='Participants' placeholder='participant1@email.co' />
+                            </List.Item>
+                            <List.Item>
+                                <Input fluid label='Subject' placeholder='Art interview' />
+                            </List.Item>
+                        </List>
+                        <Button primary onClick={this.handleSubmit}>Create</Button>
+                        <Button secondary>Cancel</Button>
+                    </Modal.Content>
+        </Modal>
+        )
+        }
 }
 
 export default CreateInterview
