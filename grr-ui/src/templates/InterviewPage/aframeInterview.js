a5c73f3e64a9d530d77d37963f0a7a26d679480e
Adam Kuhn <ark9719@rit.edu>
2018-11-07 10:40:15 -0500

    remove placeholder header

--- grr-ui/src/templates/InterviewPage/aframeInterview.js
+++ grr-ui/src/templates/InterviewPage/aframeInterview.js
@@ -12,7 +12,6 @@ class AframeInterview extends Component {
     render() {
         return (
             <div className="aframeContainer">
-            <Header as='h3'>Aframe</Header>
             <a-scene className='aframeComponent' embedded> 
                 <a-entity id="box" geometry="primitive: box" material="color: red"></a-entity>
                 <a-entity environment="preset: forest; dressingAmount: 500"></a-entity>
